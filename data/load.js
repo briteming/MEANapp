6118bc500ad5dc4edd19ac8b1d1a6797d0d1851c
ylxdzsw <ylxdzsw@gmail.com>
2014-09-11 19:38:05 +0800

    save before zuo si

--- data/load.js
+++ data/load.js
@@ -9,9 +9,10 @@ var monk = require('monk');
 var db = monk(consts.db.url);
 
 var load = function(type,query,callback){
-	db.get(type).find(query,function(err,docs){
+	console.log(query);
+	db.get(type).find({t01:query.name},function(err,content){
 		if(callback){
-			callback(err,type,docs);
+			callback(err,type,content);
 		}else{
 			debug.exception("Queried but no callback (load.js - 2895)");
 		}
--- webserver/routes/index.js
+++ webserver/routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var ds = require('../../datascheme');
+var ds = require('../../data/datascheme');
 var consts = require('../../constants');
 var save = require('../../data/save.js');
 var load = require('../../data/load.js');
@@ -13,6 +13,16 @@ router.post('/save',function(req,res){
 	save(req.query.type,req.body,function(err,type,content){
 		res.end(JSON.stringify({
 			err:err
+		}));
+	});
+});
+
+router.post('/load',function(req,res){
+	load('town',req.body,function(err,type,content){
+		console.log(content);
+		res.end(JSON.stringify({
+			err:err,
+			content:content
 		}));
 	});
 });
